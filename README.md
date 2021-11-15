b3c8063f2ff8e06743e578671f5c9fc44f667034
Ac-Kevin <44227678+Ac-Kevin@users.noreply.github.com>
2021-11-15 21:14:54 +0800

    Pull

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # alird
+
+# 快速启动
+
+```
+cd docker
+docker-compose up
+```
+
+
+# 说明(explain)
+
+将本地的公网IP更新至阿里云域名解析
+
+`update the local public network IP to the resolution operator`
+
+<br />
+<br />
+
+# 配置文件(config.ini)
+
+配置文件默认读取位置 : `./alird/config.ini`
+
+可以使用 `CONFIG_FILE` 环境变量修改位置
+
+```
+[domain]
+name=域名
+host=需要解析的关键字，如 www 和 api。多个host用;分割。
+```
+如
+```
+[domain]
+;域名
+name=china.cn
+; 使用;分割RR 如： www;www2
+host=www;
+```
+
+# 日志(logs)
+
+默认输出日志位置： `./alird/logs`
+
+> 保留 <em>7</em> 天内日志
+
+
+# 环境变量(environment)
+
+| Name            | Value                                                                                       |
+| --------------- | ------------------------------------------------------------------------------------------- |
+| ALI_ACCESSKEYID  | 阿里云 AccessKey ID ，需要登录阿里云后台获取。 [点击这里获取](https://ram.console.aliyun.com/overview)，注意配置域名权限 |
+| ALI_ACCESSKEY_SECRET | 阿里云api 密钥,同上方获取方式，创建 AccessKey ID 时出现 <em>一次</em> ，请务必保存。                                                      |
+|CONFIG_FILE     | 配置文件路径                                                                                |
+|LOG_FILEDIR|日志存储位置|
+|INTERVAL_TIME|检测间隔时间 (s) 默认30s|
+# 映射文件和文件夹
+
+| 类型   | 本机地址           | 容器地址          |
+| ------ | :----------------- | :---------------- |
+| 文件   | ./alird/config.ini | /alird/config.ini |
+| 文件夹 | ./alird/logs       | /alird/logs       |
+
+
+
+
+
---
+++ source/engine.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"app/aliclient"
+	"app/config"
+	"app/utils"
+	"log"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/alidns"
+)
+
+// run 主运行函数
+func run(cfg *config.Config, client *aliclient.Client) {
+	domains, err := cfg.GetDoMain()
+	if err != nil {
+		log.Panic(err)
+	}
+	var lastIP string
+	func() {
+		log.Printf("Info: ========================= cron joy begin!======================")
+		tempIp, err := utils.Getnetip()
+		if err != nil {
+			return
+		}
+		if tempIp == lastIP {
+			log.Printf("Info: ip no have change = %s", lastIP)
+			return
+		}
+		log.Printf("Info: begin Update RR To IP:%s", tempIp)
+		for _, domain := range domains {
+			if len(domain.Host) == 0 {
+				log.Printf("Info: domain:%s no have host", domain.Name)
+				continue
+			}
+			for _, host := range domain.Host {
+				if host == "" {
+					continue
+				}
+				req := alidns.CreateDescribeDomainRecordsRequest()
+				req.DomainName = domain.Name
+				req.SearchMode = "EXACT"
+				req.RRKeyWord = host
+				response, err := client.DescribeDomainRecords(req)
+				if err != nil {
+					log.Printf("Error: client.DescribeDomainRecords domain:%s error:%s", domain.Name, err.Error())
+					continue
+				}
+				if len(response.DomainRecords.Record) == 0 {
+					// Add RR
+					req := alidns.CreateAddDomainRecordRequest()
+					req.Value = tempIp
+					req.Type = "A"
+					req.RR = host
+					req.DomainName = domain.Name
+					resp, err := client.AddDomainRecord(req)
+					if err != nil {
+						log.Printf("Error: client.AddDomainRecord domain:%s host:%s add fail:%s", domain.Name, host, err.Error())
+						return
+					}
+					if resp.IsSuccess() {
+						log.Printf("Info: domain:%s host:%s add success", domain.Name, host)
+						continue
+					} else {
+						log.Printf("Error: client.AddDomainRecord domain:%s error:%s", domain.Name, resp.GetHttpContentString())
+						continue
+					}
+				} else {
+					// Update
+					if response.DomainRecords.Record[0].Value == tempIp {
+						log.Printf("Info: domain:%s rr:%s no hange change", domain.Name, host)
+						continue
+					}
+					req := alidns.CreateUpdateDomainRecordRequest()
+					req.RecordId = response.DomainRecords.Record[0].RecordId
+					req.RR = response.DomainRecords.Record[0].RR
+					req.Type = response.DomainRecords.Record[0].Type
+					req.Value = tempIp
+					resp, err := client.UpdateDomainRecord(req)
+					if err != nil {
+						log.Printf("Error: client.UpdateDomainRecord domain:%s host:%s Update Fail:%s", domain.Name, host, err.Error())
+						continue
+					}
+					if resp.IsSuccess() {
+						log.Printf("Info: domain:%s host:%s update success", domain.Name, host)
+						continue
+					} else {
+						log.Printf("Error: client.UpdateDomainRecord domain:%s error:%s", domain.Name, resp.GetHttpContentString())
+						continue
+					}
+				}
+			}
+
+		}
+		log.Printf("Info: ========================= cron joy done!======================")
+		lastIP = tempIp
+	}()
+}
